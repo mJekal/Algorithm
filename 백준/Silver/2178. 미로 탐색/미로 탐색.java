4d9f735720fbb0d406aa3aa2f53a912b62844833
mJekal <132838012+mJekal@users.noreply.github.com>
2023-10-30 12:03:30 +0900

    Update 미로 탐색.java

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
 	
-	static class Pair {
+		static class Pair {
 		int x, y;
 		Pair(int x, int y){
 			this.x = x;
@@ -20,33 +20,37 @@ public class Main {
 	    		N = Integer.parseInt(st.nextToken());
 	    		M = Integer.parseInt(st.nextToken());
 	    		int[][] board = new int[N][M];
-	    		boolean[][] vis = new boolean[N][M];
+	    		int[][] dist = new int[N][M];
 	    		for (int i = 0; i<N;i++) {
 	    			String s = br.readLine();
 	    			for (int j = 0; j<M;j++) {
 	    				board[i][j] = s.charAt(j) - '0';
 	    			}
 	    		}
+	    		for (int i = 0; i<N;i++) {
+	    			for (int j = 0; j<M;j++) {
+	    				dist[i][j] = -1;
+	    			}
+	    		}
 	    		
 	    		Queue<Pair> Q = new LinkedList<>();
 	    		
-	    		vis[0][0] = true;
 	    		Q.add(new Pair(0,0));
-	    		
+	    		dist[0][0] = 0;
 	    		while(!Q.isEmpty()) {
 	    			Pair cur = Q.poll();
 	    			for(int dir = 0; dir < 4; dir++) {
 	    				int nx = cur.x + dx[dir];
 	    				int ny = cur.y + dy[dir];
 	    				if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-	    				if(board[nx][ny] == 0 || vis[nx][ny]) continue;
+	    				if(board[nx][ny] == 0 || dist[nx][ny]>=0) continue;
 	    				
-	    				vis[nx][ny] = true;
-	    				board[nx][ny] = board[cur.x][cur.y]+1;
+	    				dist[nx][ny] = dist[cur.x][cur.y]+1;
 	    				Q.add(new Pair(nx,ny));
+	    			
 	    		}
 	    		
 	    }
-	    		System.out.println(board[N-1][M-1]);
+	    		System.out.println(dist[N-1][M-1]+1);
 	}
 }
