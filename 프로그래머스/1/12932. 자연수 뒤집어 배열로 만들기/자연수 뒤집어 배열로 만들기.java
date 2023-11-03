55cf5513feaea5ca0764fea9f586d810d445bdbe
mJekal <132838012+mJekal@users.noreply.github.com>
2023-11-03 16:01:00 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.07 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,9 +1,15 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
     public int[] solution(long n) {
-        String numStr = String.valueOf(n);
-        int[] answer = new int[numStr.length()];
-        for(int i = numStr.length() - 1; i >= 0; i--){
-            answer[numStr.length() - 1 - i] = numStr.charAt(i) - '0';
+        String str = Long.toString(n);
+        String reverse = new StringBuilder(str).reverse().toString();
+        char[] arr = reverse.toCharArray();
+        
+        int[] answer = new int[arr.length];
+        for(int i = 0; i<arr.length; i++){
+            answer[i] = arr[i] - '0';
         }
         return answer;
     }
