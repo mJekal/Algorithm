b0aa7660947ff5ad6f671ad2679f1dda4df1cc2b
mJekal <132838012+mJekal@users.noreply.github.com>
2023-11-05 03:12:22 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 152 ms, Memory: 15916 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,21 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main{
-    public static void main(String[] args){
-        
-        int[] alpha = new int[26];
-
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        
-        for(int i =0; i<str.length(); i++){
-        alpha[str.charAt(i)-97]++;
-        }
-        
-        for(int i =0; i<26; i++){
-        System.out.print(alpha[i]+" ");
-        }
-        
-        
-        }
+public class Main {
+	static int[] ans;
+    public static void main(String[] args) throws IOException {
+    	ans = new int[26];
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	String s = br.readLine();
+    	for(char c : s.toCharArray()) {
+    		int index = c-'a';
+    		ans[index] += 1;
+    	}
+    	for(int i = 0; i<26; i++) {
+    		System.out.print(ans[i]+" ");
+    	}
+    }
 }
