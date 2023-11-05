2130fa67276a6bde507e1b02bbe57ffe3f50bb8e
mJekal <132838012+mJekal@users.noreply.github.com>
2023-11-05 16:23:49 +0900

    [Silver III] Title: N과 M (2), Time: 156 ms, Memory: 15892 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int n, m;
+	static int[] arr;
+	static boolean[] isused;
+
+	static void backtracking(int k, int index) {
+		if (k == m) {
+			for (int i = 0; i < m; i++)
+				System.out.print(arr[i] + " ");
+			System.out.println();
+			return;
+		}
+
+		for (int i = index; i <= n; i++) {
+			if (!isused[i]) {
+				arr[k] = i;
+				isused[i] = true;
+				backtracking(k + 1, i);
+				isused[i] = false;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[m];
+		isused = new boolean[n + 1];
+		backtracking(0,1);
+	}
+}
