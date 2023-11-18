6dccc2166e3ef86f05db3e7531cb109408ec3052
mJekal <132838012+mJekal@users.noreply.github.com>
2023-11-18 14:30:42 +0900

    [Silver I] Title: 나이트의 이동, Time: 492 ms, Memory: 117996 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -5,65 +5,54 @@ public class Main {
 
 	static class Pair {
 		int x, y;
-		Pair(int x,int y){
+
+		Pair(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
-		
 	}
-	
-	static int[] dx = {2,1,-1,-2,-2,-1, 1, 2};
-	static int[] dy = {1,2, 2, 1,-1,-2,-2,-1};
-	
-	public static void main(String[] args) throws IOException  {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for(int tc=0; tc<T;tc++) {
-			int I = Integer.parseInt(br.readLine());
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			Queue<Pair> Q = new LinkedList<>();
-			
-			int[][] dist = new int[I][I] ;
-			
-			for(int i = 0; i<I; i++) {
-				for(int j = 0; j<I; j++) {
-				dist[i][j] = -1;
-			}
-			}
-			
-			int kk = Integer.parseInt(st.nextToken());
-			int tt = Integer.parseInt(st.nextToken());
-			
-			st = new StringTokenizer(br.readLine());
-			int k = Integer.parseInt(st.nextToken());
-			int t = Integer.parseInt(st.nextToken());
 
-			dist[kk][tt] = 0;
-			Q.add(new Pair(kk,tt));
-			
-			while(!Q.isEmpty()) {
-			Pair cur = Q.poll();
-			for(int dir=0; dir<8; dir++) {			
-				int ny = cur.y + dy[dir];
-				int nx = cur.x + dx[dir];
-			
-			if(nx<0||nx>=I||ny<0||ny>=I) continue;
-			if(dist[nx][ny]!=-1) continue;
-				dist[nx][ny] = dist[cur.x][cur.y]+1;
-				Q.add(new Pair(nx,ny));
-				
+	static int[][] dist;
+	static int N, T;
+	static int nn, mm;
+	static int[] dx = { 1, 1, -1, -1, 2, 2, -2, -2 };
+	static int[] dy = { 2, -2, 2, -2, 1, -1, 1, -1 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		for (int tc = 0; tc < T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			dist = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					dist[i][j] = -1;
+				}
 			}
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			dist[n][m] = 0;
+			Queue<Pair> q = new LinkedList<>();
+			q.add(new Pair(n, m));
+			StringTokenizer st1 = new StringTokenizer(br.readLine());
+			nn = Integer.parseInt(st1.nextToken());
+			mm = Integer.parseInt(st1.nextToken());
+
+			while (!q.isEmpty()) {
+				Pair cur = q.poll();
+				for (int dir = 0; dir < 8; dir++) {
+					int nx = dx[dir] + cur.x;
+					int ny = dy[dir] + cur.y;
+					if (nx < 0 || nx >= N || ny < 0 || ny >= N)
+						continue;
+					if (dist[nx][ny] >= 0)
+						continue;
+					dist[nx][ny] = dist[cur.x][cur.y] + 1;
+					q.add(new Pair(nx, ny));
+				}
 			}
-			
-			
-			
-	System.out.println(dist[k][t]);	
+			System.out.println(dist[nn][mm]);
+		}
 	}
-	
-	}
-	
-	
-	
-	
 }
