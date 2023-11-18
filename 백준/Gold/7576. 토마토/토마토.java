5fe19c397ed4a496f7812ef6f925d8cbb8f215cf
mJekal <132838012+mJekal@users.noreply.github.com>
2023-11-18 12:17:33 +0900

    [Gold V] Title: 토마토, Time: 624 ms, Memory: 123908 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,13 +2,13 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N;
-	static int M;
+
+	static int max = Integer.MIN_VALUE;
+	static int N, M;
 	static int[][] board;
 	static int[][] dist;
 	static int[] dx = { 1, 0, -1, 0 };
 	static int[] dy = { 0, 1, 0, -1 };
-	static int max = Integer.MIN_VALUE;
 
 	static class Pair {
 		int x, y;
@@ -19,7 +19,9 @@ public class Main {
 		}
 	}
 
-	public static void main(String args[]) throws IOException {
+	static Queue<Pair> q = new LinkedList<>();
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		M = Integer.parseInt(st.nextToken());
@@ -27,14 +29,14 @@ public class Main {
 
 		board = new int[N][M];
 		dist = new int[N][M];
-		Queue<Pair> q = new LinkedList<>();
+
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				board[i][j] = Integer.parseInt(st.nextToken());
 				dist[i][j] = -1;
 				if (board[i][j] == -1) {
-					dist[i][j] = -2;
+					dist[i][j] = 0;
 				}
 				if (board[i][j] == 1) {
 					dist[i][j] = 0;
@@ -46,26 +48,28 @@ public class Main {
 		while (!q.isEmpty()) {
 			Pair cur = q.poll();
 			for (int dir = 0; dir < 4; dir++) {
-				int nx = cur.x + dx[dir];
-				int ny = cur.y + dy[dir];
+				int nx = dx[dir] + cur.x;
+				int ny = dy[dir] + cur.y;
+
 				if (nx < 0 || nx >= N || ny < 0 || ny >= M)
 					continue;
-				if (board[nx][ny] != 0 || dist[nx][ny] != -1)
+				if (dist[nx][ny] >= 0)
 					continue;
+
 				dist[nx][ny] = dist[cur.x][cur.y] + 1;
 				q.add(new Pair(nx, ny));
 			}
 		}
+
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				if (dist[i][j] == -1) {
 					System.out.println(-1);
 					return;
 				}
-				max = Math.max(dist[i][j], max);
+				max = Math.max(max, dist[i][j]);
 			}
 		}
-
 		System.out.println(max);
 	}
 }
