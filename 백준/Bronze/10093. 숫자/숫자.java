bf5690a80a62d10e214c43ea041a4cc4d9c9cd7b
mJekal <132838012+mJekal@users.noreply.github.com>
2023-12-24 05:00:39 +0900

    [Bronze II] Title: 숫자, Time: 184 ms, Memory: 15892 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long A = Integer.parseInt(st.nextToken());
+		long B = Integer.parseInt(st.nextToken());
+		if (A > B) {
+			long temp = A;
+			A = B;
+			B = temp;
+		}
+		if (B - A - 1 == -1) {
+			A = A - 1;
+		}
+		System.out.println(B - A - 1);
+		for (long i = (A + 1); i < B; i++) {
+			System.out.print(i + " ");
+		}
+	}
+}
