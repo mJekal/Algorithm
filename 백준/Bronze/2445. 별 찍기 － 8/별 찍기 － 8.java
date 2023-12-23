efd90d3d8aaaf720f9dd13de49351f13c651418e
mJekal <132838012+mJekal@users.noreply.github.com>
2023-12-24 04:39:11 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 548 ms, Memory: 22684 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				System.out.print('*');
+			}
+			for (int j = 0; j < 2 * N - 2 * (i + 1); j++) {
+				System.out.print(' ');
+			}
+			for (int j = 0; j < i + 1; j++) {
+				System.out.print('*');
+			}
+			System.out.println(' ');
+		}
+		for (int i = 0; i < N-1; i++) {
+			for (int j = N-i-1; j > 0; j--) {
+				System.out.print('*');
+			}
+			for (int j = 2 * (i + 1); j > 0; j--) {
+				System.out.print(' ');
+			}
+			for (int j = N-i-1; j > 0; j--) {
+				System.out.print('*');
+			}
+			System.out.println(' ');
+		}
+	}
+}
