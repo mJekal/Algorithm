6df0b98f0347dcbac0ac9edad4ee97f86ee64616
mJekal <132838012+mJekal@users.noreply.github.com>
2023-12-24 01:06:30 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,10 +1,11 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
-    }
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
 }
-
-
