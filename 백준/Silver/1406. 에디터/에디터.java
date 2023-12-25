cdf633ef4d50c9a2e56fe916967174cb5945be4e
mJekal <132838012+mJekal@users.noreply.github.com>
2023-12-26 04:51:29 +0900

    [Silver II] Title: 에디터, Time: 428 ms, Memory: 54276 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -2,49 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	static LinkedList<Character> editor = new LinkedList<>();
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String ori = br.readLine();		
+		String s = br.readLine();
+		for (char c : s.toCharArray()) {
+			editor.add(c);
+		}
+		ListIterator<Character> cursor = editor.listIterator();
+		while (cursor.hasNext()) {
+			cursor.next();
+		}
 		int M = Integer.parseInt(br.readLine());
-		
-		LinkedList<Character> ori_c = new LinkedList<>();
-		for(int i = 0; i < ori.length(); i++) {
-			ori_c.add(ori.charAt(i));
-		}
-		ListIterator<Character> iter = ori_c.listIterator();
-		while(iter.hasNext()) {
-			iter.next();
-		}
 		for (int i = 0; i < M; i++) {
 			String inst = br.readLine();
 			switch (inst.charAt(0)) {
 			case 'P':
-				char tok = inst.charAt(2);
-				iter.add(tok);
+				cursor.add(inst.charAt(2));
 				break;
 			case 'L':
-				if (iter.hasPrevious())
-					iter.previous();
+				if (cursor.hasPrevious())
+					cursor.previous();
 				break;
 			case 'D':
-				if (iter.hasNext())
-					iter.next();
+				if (cursor.hasNext())
+					cursor.next();
 				break;
 			case 'B':
-				if (iter.hasPrevious()) {
-					iter.previous();
-					iter.remove();
+				if (cursor.hasPrevious()) {
+					cursor.previous();
+					cursor.remove();
 				}
-				break;
-			default:
 				break;
 			}
 		}
-		for(Character chr : ori_c) {
-			bw.write(chr);
+		for (char c : editor) {
+			bw.write(c);
 		}
 		bw.flush();
-		bw.close(); 
+		bw.close();
 	}
 }
