b4a167816afdaed8094195867ebeb6894c502f22
mJekal <132838012+mJekal@users.noreply.github.com>
2023-12-28 03:37:58 +0900

    [Silver II] Title: 유기농 배추, Time: 160 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,9 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int[] dx = { 1, 0, -1, 0 };
-	static int[] dy = { 0, 1, 0, -1 };
-
 	static class Pair {
 		int x, y;
 
@@ -14,68 +11,58 @@ public class Main {
 		}
 	}
 
+	static int[] dx = { 1, 0, -1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
+	static int cnt;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int T = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		for (int tc = 0; tc < T; tc++) {
-			st = new StringTokenizer(br.readLine());
-
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < T; i++) {
+			Queue<Pair> q = new LinkedList<>();
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int M = Integer.parseInt(st.nextToken());
 			int N = Integer.parseInt(st.nextToken());
 			int K = Integer.parseInt(st.nextToken());
-
-			Queue<Pair> Q = new LinkedList<>();
-
 			int[][] board = new int[M][N];
-			int[][] dist = new int[M][N];
-
-			for (int i = 0; i < M; i++) {
-				for (int j = 0; j < N; j++) {
-					board[i][j] = 0;
-					dist[i][j] = -1;
-				}
-			}
-
-			for (int i = 0; i < K; i++) {
+			boolean[][] vis = new boolean[M][N];
+			for (int j = 0; j < K; j++) {
 				st = new StringTokenizer(br.readLine());
-				int I = Integer.parseInt(st.nextToken());
-				int J = Integer.parseInt(st.nextToken());
-				board[I][J] = 1;
+				int k = Integer.parseInt(st.nextToken());
+				int m = Integer.parseInt(st.nextToken());
+				board[k][m] = 1;
 			}
 
-			int cnt = 0;
-			for (int i = 0; i < M; i++) {
-				for (int j = 0; j < N; j++) {
-					if (board[i][j] == 0 || dist[i][j] != -1) continue;
-						dist[i][j] = 0;
-						Q.add(new Pair(i, j));
-
-						while (!Q.isEmpty()) {
-							Pair cur = Q.poll();
-							for (int dir = 0; dir < 4; dir++) {
-								int nx = cur.x + dx[dir];
-								int ny = cur.y + dy[dir];
-
-								if (nx < 0 || nx >= M || ny < 0 || ny >= N)
-									continue;
-								if (board[nx][ny] != 1 || dist[nx][ny] != -1)
-									continue;
-
-								dist[nx][ny] = dist[cur.x][cur.y] + 1;
-								Q.add(new Pair(nx, ny));
-							}
-						}
-                        
-					
+			for (int I = 0; I < M; I++) {
+				for (int J = 0; J < N; J++) {
+					if (vis[I][J] == true || board[I][J] != 1)
+						continue;
+					vis[I][J] = true;
+					q.add(new Pair(I, J));
 					cnt++;
+					while (!q.isEmpty()) {
+						Pair cur = q.poll();
+						for (int dir = 0; dir < 4; dir++) {
+							int nx = cur.x + dx[dir];
+							int ny = cur.y + dy[dir];
+
+							if (nx < 0 || nx >= M || ny < 0 || ny >= N)
+								continue;
+							if (vis[nx][ny] == true || board[nx][ny] != 1)
+								continue;
+
+							vis[nx][ny] = true;
+							q.add(new Pair(nx, ny));
+						}
+					}
+
 				}
 			}
-
-			System.out.println(cnt);
-
+			sb.append(cnt);
+			sb.append("\n");
+			cnt = 0;
 		}
+		System.out.println(sb);
 	}
-
 }
