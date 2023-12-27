9f087786676cfea1864fdeae158eb051ae5d800d
mJekal <132838012+mJekal@users.noreply.github.com>
2023-12-28 06:33:25 +0900

    [Gold V] Title: 토마토, Time: 732 ms, Memory: 124432 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -2,15 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-	static int max = Integer.MIN_VALUE;
-	static int N, M, H;
-	static int[][][] board;
-	static int[][][] dist;
-	static int[] dx = { 1, 0, -1, 0, 0, 0 };
-	static int[] dy = { 0, 1, 0, -1, 0, 0 };
-	static int[] dz = { 0, 0, 0, 0, 1, -1 };
-
 	static class Pair {
 		int x, y, z;
 
@@ -21,29 +12,31 @@ public class Main {
 		}
 	}
 
+	static int[][][] board;
+	static int[][][] dist;
+	static int[] dx = { 1, 0, -1, 0, 0, 0 };
+	static int[] dy = { 0, 1, 0, -1, 0, 0 };
+	static int[] dz = { 0, 0, 0, 0, 1, -1 };
+	static int max = Integer.MIN_VALUE;
 	static Queue<Pair> q = new LinkedList<>();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
-		H = Integer.parseInt(st.nextToken());
-
-		board = new int[N][M][H];
-		dist = new int[N][M][H];
-
-		for (int k = 0; k < H; k++) {
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		board = new int[N][M][K];
+		dist = new int[N][M][K];
+		for (int k = 0; k < K; k++) {
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < M; j++) {
 					board[i][j][k] = Integer.parseInt(st.nextToken());
-					dist[i][j][k] = -1;
-					if (board[i][j][k] == -1) {
-						dist[i][j][k] = 0;
+					if (board[i][j][k] == 0) {
+						dist[i][j][k] = -1;
 					}
 					if (board[i][j][k] == 1) {
-						dist[i][j][k] = 0;
 						q.add(new Pair(i, j, k));
 					}
 				}
@@ -53,13 +46,13 @@ public class Main {
 		while (!q.isEmpty()) {
 			Pair cur = q.poll();
 			for (int dir = 0; dir < 6; dir++) {
-				int nx = dx[dir] + cur.x;
-				int ny = dy[dir] + cur.y;
-				int nz = dz[dir] + cur.z;
+				int nx = cur.x + dx[dir];
+				int ny = cur.y + dy[dir];
+				int nz = cur.z + dz[dir];
 
-				if (nx < 0 || nx >= N || ny < 0 || ny >= M || nz < 0 || nz >= H)
+				if (nx < 0 || nx >= N || ny < 0 || ny >= M || nz < 0 || nz >= K)
 					continue;
-				if (dist[nx][ny][nz] >= 0)
+				if (board[nx][ny][nz] != 0 || dist[nx][ny][nz] >= 0)
 					continue;
 
 				dist[nx][ny][nz] = dist[cur.x][cur.y][cur.z] + 1;
@@ -69,9 +62,9 @@ public class Main {
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				for (int k = 0; k < H; k++) {
+				for (int k = 0; k < K; k++) {
 					if (dist[i][j][k] == -1) {
-						System.out.println(-1);
+						System.out.println("-1");
 						return;
 					}
 					max = Math.max(max, dist[i][j][k]);
@@ -81,3 +74,4 @@ public class Main {
 		System.out.println(max);
 	}
 }
+
