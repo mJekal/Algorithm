1225c3aa7ce3d05a709bb132258217737ee48fca
mJekal <132838012+mJekal@users.noreply.github.com>
2024-01-06 22:23:02 +0900

    [Silver III] Title: N과 M (1), Time: 2008 ms, Memory: 52372 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,35 +2,36 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	    static int n, m;
-	    static int[] arr;
-	    static boolean[] isused;
 
-	    static void backtracking(int k) {
-	        if (k == m) {
-	            for (int i = 0; i < m; i++)
-	                System.out.print(arr[i] + " ");
-	            System.out.println();
-	            return;
-	        }
+	static int N, M;
+	static int[] arr;
+	static boolean[] isUsed;
 
-	        for (int i = 1; i <= n; i++) {
-	            if (!isused[i]) {
-	                arr[k] = i;
-	                isused[i] = true;
-	                backtracking(k + 1);
-	                isused[i] = false;
-	            }
-	        }
-	    }
-
-	    public static void main(String[] args) throws IOException {
-	    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    	StringTokenizer st = new StringTokenizer(br.readLine());
-	    	n = Integer.parseInt(st.nextToken());
-	      	m= Integer.parseInt(st.nextToken());
-	        arr = new int[m];
-	        isused = new boolean[n + 1];
-	        backtracking(0);
-	    }
+	static void func(int k) {
+		if (k == M) {
+			for (int i = 0; i < M; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			if (!isUsed[i]) {
+				arr[k] = i;
+				isUsed[i] = true;
+				func(k + 1);
+				isUsed[i] = false;
+			}
+		}
 	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[M];
+		isUsed = new boolean[N + 1];
+		func(0);
+	}
+}
