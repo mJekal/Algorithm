f1c7353cb3a5f9dad6495f533d109c4cbd3399cf
mJekal <132838012+mJekal@users.noreply.github.com>
2024-01-18 20:09:21 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,9 +1,13 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        for(int i = 0 ; i<absolutes.length; i++){
-            if(signs[i]) answer+= absolutes[i];
-            else answer-= absolutes[i];
+        for(int i = 0; i<signs.length; i++){
+            if(signs[i]==false){
+                absolutes[i] *= -1;
+            }
+        }
+        for(int num: absolutes){
+            answer += num;
         }
         return answer;
     }
