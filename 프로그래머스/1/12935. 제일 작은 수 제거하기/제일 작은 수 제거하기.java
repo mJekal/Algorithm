01320fa56a2ce9fee95137b4f26574d16923ca8c
mJekal <132838012+mJekal@users.noreply.github.com>
2024-01-18 20:45:28 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.11 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -2,21 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-       
-        List<Integer> arr2 = new LinkedList<>();
-        int min = Integer.MAX_VALUE;
-        for(int num: arr){
-            if(num < min) {min = num;};
-            arr2.add(num);
+        int index =0;
+        if(arr.length==1){
+            return new int[] {-1};
         }
-        arr2.remove(Integer.valueOf(min));
-        if(arr.length == 1) return new int[] {-1};
-        int[] result = new int[arr2.size()];
-        for(int i = 0; i<result.length; i++){
-            result[i] = arr2.get(i);
+        int[] answer = new int[arr.length-1];
+        int mn = Integer.MAX_VALUE;
+        for(int num : arr){
+            mn = Math.min(mn, num);
         }
-    
-   
-    return result;
-     }     
+        for(int i = 0; i<arr.length; i++){
+            if(mn == arr[i]){
+                continue;
+            }
+            answer[index++] = arr[i];
+        }
+        return answer;
+    }
 }
