1226efcd35e5a162fcdd7d5789cae807e9aae398
mJekal <132838012+mJekal@users.noreply.github.com>
2024-01-18 20:19:44 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 9.24 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,12 +1,15 @@
 class Solution {
     public String solution(String[] seoul) {
-        String answer = "김서방은 ";
-        for(int i = 0; i<seoul.length; i++){
-            if (seoul[i].equals("Kim")) {
-            answer += i;
-            break;}
+        String answer = "";
+        int k = 0;
+        int ans = 0;
+        for(String str : seoul){
+            if(str.equals("Kim")){
+                ans = k; 
+            }
+            k++;
         }
-        answer += "에 있다";
+        answer ="김서방은 " + Integer.toString(ans) + "에 있다";
         return answer;
     }
 }
