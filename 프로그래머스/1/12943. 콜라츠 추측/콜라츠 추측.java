76e70c0d608aeb41af550dacef29e106d7bf9e26
mJekal <132838012+mJekal@users.noreply.github.com>
2024-01-18 20:16:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,14 +1,21 @@
 class Solution {
     public int solution(int num) {
         int answer = 0;
-        long num1 = num;
-        while(num1 != 1){
-        if(num1 %2==0) {num1 = num1 / 2;}
-        else {num1 = num1*3+1;}
-        answer++;
-        if (answer >= 500) return -1;}
-        
+        if(num == 1){
+            return 0;
+        }
+        long temp = (long) num;
+        while(temp != 1){
+            if(temp % 2 == 0){
+                temp /= 2;
+            }else{
+                temp = temp * 3 + 1;
+            }
+            answer++;
+        }
+        if(answer>=500){
+            answer = -1;
+        }
         return answer;
     }
 }
-
