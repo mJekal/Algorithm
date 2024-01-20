a5663699c4fcedd19f33da5ae6e39792ed2db870
mJekal <132838012+mJekal@users.noreply.github.com>
2024-01-20 12:57:49 +0900

    [level 1] Title: 시저 암호, Time: 42.77 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,24 +1,15 @@
 class Solution {
-   
-    char push(char c, int n){
-        char offset = Character.isUpperCase(c)? 'A':'a';
-        if(c != ' ') {
-            c = (char)((c - offset + n) % 26 + offset);
-            if (c < offset) {
-                c += 26;
-            }
-            return c;
-        }
-        return c;
-    }
-    
     public String solution(String s, int n) {
-        char[] split = s.toCharArray();
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < split.length; i++){
-            sb.append(push(split[i], n));
+        String answer = "";
+        for(int i =0; i<s.length(); i++){
+                char ch = s.charAt(i);
+              if(Character.isLowerCase(ch)) { 
+                  ch = (char) ((ch - 'a' + n) % 26 + 'a');
+              } else if(Character.isUpperCase(ch)) {
+                  ch = (char) ((ch - 'A' + n) % 26 + 'A');
+              }
+              answer += ch;
         }
-        return sb.toString();
+        return answer;
     }
 }
-
