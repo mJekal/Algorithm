27d0e56ca88693bdd9b3be39e6c369ccc132b2fb
mJekal <132838012+mJekal@users.noreply.github.com>
2024-02-03 12:25:04 +0900

    [level 1] Title: 모의고사, Time: 0.80 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,30 +1,45 @@
-import java.util.stream.IntStream;
+import java.util.*; 
 
 class Solution {
-    static int[] answer;
-    static int min_size,index, cnt;
-    static int[][] person = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
-
-   static int count(int[] answers, int[] person) {
-        for (int i = 0; i < answers.length; i++) {
-            if (index > person.length - 1) { index = index % person.length; }
-            if (answers[i] == person[index++]) {
-                
+    
+    static int[] num1 = {1,2,3,4,5};
+    static int[] num2 = {2,1,2,3,2,4,2,5};
+    static int[] num3 = {3,3,1,1,2,2,4,4,5,5};
+    
+    public int[] solution(int[] answers) {
+        int[] answer = new int[3];
+        int index =0;int cnt = 0;
+        int sc1 = 0;
+        int sc2 = 0;
+        int sc3 = 0;
+        for(int i = 0; i<answers.length; i++){
+           if(num1[i%num1.length] == answers[i]){
+                sc1++;
+            }
+            if(num2[i%num2.length] == answers[i]){
+                sc2++;
+            }
+            if(num3[i%num3.length] == answers[i]){
+                sc3++;
+            }}
+        answer[0] = sc1;
+        answer[1] = sc2;
+        answer[2] = sc3;
+        int mx = -1;
+        for(int i =0 ; i<3; i++){
+            mx = Math.max(mx,answer[i]);
+        }
+        for(int i =0 ; i<3; i++){
+            if(answer[i]==mx){
                 cnt++;
             }
         }
-        return cnt;
-    }
-    
-    public int[] solution(int[] answers) {
-        min_size = 0;
-        answer = new int[3];
-        for (int i = 0; i < 3; i++) {
-            answer[i] = count(answers, person[i]);
-            index = 0;
-            cnt = 0;
-            if (min_size < answer[i]) {min_size = answer[i];}
+        int[] ans = new int[cnt];
+        for(int i =0 ; i<3; i++){
+            if(answer[i]==mx){
+               ans[index++] = i+1;
+            }
         }
-        return IntStream.range(0,3).filter(i->answer[i]==min_size).map(i->i+1).toArray();
+            return ans;
     }
 }
