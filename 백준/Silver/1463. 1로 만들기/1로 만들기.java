fb9f08e0f8d57535c1b0f85e726f3cd608835cb4
mJekal <132838012+mJekal@users.noreply.github.com>
2024-02-05 01:04:22 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18100 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,22 +3,23 @@ import java.util.*;
 
 public class Main {
 
-	static int[] dp;
+	static int[] dp = new int[1000001];
 	static int mn;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		dp = new int[N + 1];
 		dp[1] = 0;
 		for (int i = 2; i <= N; i++) {
-			dp[i] = dp[i - 1] + 1;
+			mn = Integer.MAX_VALUE;
 			if (i % 3 == 0) {
-				dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+				mn = Math.min(dp[i / 3] + 1, mn);
 			}
 			if (i % 2 == 0) {
-				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+				mn = Math.min(dp[i / 2] + 1, mn);
 			}
+			mn = Math.min(dp[i - 1] + 1, mn);
+			dp[i] = mn;
 		}
 		System.out.println(dp[N]);
 	}
