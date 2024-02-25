93188adca1e99dcd702c53747f294ed210c67400
mJekal <132838012+mJekal@users.noreply.github.com>
2024-02-25 22:16:53 +0900

    [level 1] Title: 문자열 나누기, Time: 0.49 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -2,28 +2,19 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
-        char x = s.charAt(0);
-        String cpy = s;
-        int same = 1; int diff = 0; int temp = 0;
-        for(int i = 1; i<s.length(); i++){ 
-            if(same==diff){answer++;
-                cpy = cpy.substring(i-temp);
-                same = 0; diff=0;
-                x=s.charAt(i);
-                temp = i;
-            }
-            if(x==s.charAt(i)){
-                same++;
-            }else{
-                diff++;
-            }
-
-        }
-       System.out.println(cpy+" "+same+" "+diff);
-         if(same==diff){answer++;}
-        if(cpy.length() != 0 && same!=diff){answer++;}
-        return answer;
-    }
+   	public int solution(String s) {
+		char first = s.charAt(0);
+		int firstNum = 0;
+		int diff = 0;
+		int result = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (firstNum == diff) {
+				result++;
+				first = s.charAt(i);
+			}
+			if (s.charAt(i) == first) firstNum++;
+			else diff++;
+		}
+		return result;
+	}
 }
