04be48299bf3ad47b41333ab84f4dad509e7b777
mJekal <132838012+mJekal@users.noreply.github.com>
2024-02-26 00:45:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.85 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,19 +1,23 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
-	Map<String, Integer> map = new HashMap<>();
-
-	public String solution(String[] participant, String[] completion) {
-		for (int i = 0; i < participant.length; i++) {
-			map.putIfAbsent(participant[i], 0);
-			map.put(participant[i], map.get(participant[i]) + 1);
-		}
-		for (int i = 0; i < completion.length; i++) {
-			map.put(completion[i], map.get(completion[i]) - 1);
-			if (map.get(completion[i]) == 0) {
-				map.remove(completion[i]);
-			}
-		}
-		return map.keySet().iterator().next();
-	}
+    static HashMap<String, Integer> map = new HashMap<>();
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        for(String part : participant){
+            map.put(part, map.getOrDefault(part,0)+1);
+        }
+        for(String part : completion){
+            map.put(part, map.get(part)-1);
+        }
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+        while(iter.hasNext()){
+            Map.Entry<String, Integer> entry = iter.next();
+            if(entry.getValue() != 0){
+                return entry.getKey();
+            }
+        }
+        return answer;
+    }
 }
