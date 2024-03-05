07470645593be40752f51c4cb782aea0d2398d73
mJekal <132838012+mJekal@users.noreply.github.com>
2024-03-05 18:32:46 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 1.14 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,40 +1,30 @@
 import java.util.*;
-class Solution {
-    int sort_by_Num = 0; 
-    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        
-        String[] dataArr = {"code","date","maximum","remain"};
-        int extNum = 0;       
-        for(int i =0; i<dataArr.length; i++) {
-            if(dataArr[i].equals(ext)) {
-                extNum = i;
-            }
-            if(dataArr[i].equals(sort_by)) {
-                sort_by_Num = i;
-            }
-        }
-        
-        List<Integer[]> dataList = new ArrayList<>();
-        for(int i =0; i<data.length; i++) {
-            if(data[i][extNum] < val_ext) {
-                dataList.add(Arrays.stream(data[i]).boxed().toArray(Integer[]::new));
-            }
-        }
-        
-        Collections.sort(dataList, new Comparator<Integer[]>() {
-            @Override
-            public int compare(Integer[] arr1, Integer[] arr2) {
-                return Integer.compare(arr1[sort_by_Num], arr2[sort_by_Num]);
-            }
-        });
 
-        int[][] answer = new int[dataList.size()][dataList.get(0).length];
-        for (int i = 0; i < dataList.size(); i++) {
-            answer[i] = Arrays.stream(dataList.get(i))
-                               .mapToInt(Integer::intValue)
-                               .toArray();
+class Solution {
+    static int idx = 0;
+    static int exts = 0;
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        String[] sorts = {"code", "date", "maximum", "remain"};
+        List<int[]> datas = new ArrayList<>(Arrays.asList(data));
+        for (int i = 0; i < 4; i++) {
+            if (sorts[i].equals(sort_by)) {  
+                idx = i;
+            }
+            if (sorts[i].equals(ext)) {  
+                exts = i;
+            }
         }
-        
+        List<int[]> filter = new ArrayList<>();
+        for (int[] row : datas) {
+            if (row[exts] < val_ext) {
+                filter.add(row);
+            }
+        }
+        Collections.sort(filter, (o1, o2) -> o1[idx] - o2[idx]);
+        int[][] answer = new int[filter.size()][data[0].length];
+        for (int i = 0; i < filter.size(); i++) {
+            answer[i] = filter.get(i);
+        }
         return answer;
     }
 }
