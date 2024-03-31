f51060a5432e9b1c00f61e48df6006f2821687c9
mJekal <132838012+mJekal@users.noreply.github.com>
2024-04-01 08:01:21 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,45 +1,37 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int sum;
-    static int[] arr = new int[9];
-    static int[] arr2 = new int[7];
-    static int iindex;
-    static int jindex;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i < 9; i++) {
-            int k = Integer.parseInt(br.readLine());
-            arr[i] = k;
-        }
+	static List<Integer> arr = new ArrayList<>();
 
-        int index;
-        for (int i = 0; i < 9; i++) {
-            for (int j = i + 1; j < 9; j++) {
-                sum = 0;
-                index = 0;
-                iindex = i;
-                jindex = j;
-                for (int k = 0; k < 9; k++) {
-                    if (k == iindex || k == jindex) {
-                        continue;
-                    }
-                    sum += arr[k];
-                    arr2[index++] = arr[k];
-                }
-                Arrays.sort(arr2);
-                if (sum == 100) {
-                    break;
-                }
-            }
-            if (sum == 100) {
-                break;
-            }
-        }
-        for (int i = 0; i < 7; i++) {
-            System.out.println(arr2[i]);
-        }
-    }
+	static void print(int k, int h) {
+		for (int i = 0; i < 9; i++) {
+			if (i == h || i == k)
+				continue;
+			System.out.println(arr.get(i));
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int sum = 0;
+		for (int i = 0; i < 9; i++) {
+			arr.add(Integer.parseInt(br.readLine()));
+			sum += arr.get(i);
+		}
+		int k = sum - 100;
+		Collections.sort(arr);
+		for (int i = 0; i < 8; i++) {
+			for (int j = i + 1; j < 9; j++) {
+				if (arr.get(i) + arr.get(j) == k) {
+					print(i, j);
+					return;
+				}
+				;
+			}
+		}
+
+	}
 }
