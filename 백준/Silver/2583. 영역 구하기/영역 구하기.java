a06df916fe7cbc9c018ade34e9457e9a8c91b17a
mJekal <132838012+mJekal@users.noreply.github.com>
2024-05-11 00:58:40 +0900

    [Silver I] Title: 영역 구하기, Time: 132 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -2,69 +2,84 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static final int dx[] = {0,0,1,-1};
-	static final int dy[] = {1,-1,0,0};
-	static int n,m,count;
-	static int square[][];
+
+	static class Pair {
+		int x, y;
+
+		Pair(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	static int cnt = 0;
+	static int area = 0;
+
+	static int[] dx = { 0, 1, 0, -1 };
+	static int[] dy = { -1, 0, 1, 0 };
+	static int mx = -1;
+	static int[][] board;
+	static boolean[][] vis;
+	static int M, N, K;
+	static Queue<Pair> q = new LinkedList<>();
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		
-		square = new int[n][m];
-		
-		for(int i=0; i<k; i++) {
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
+		board = new int[N][M];
+		vis = new boolean[N][M];
+		List<Integer> a = new LinkedList<>();
+		for (int tc = 0; tc < K; tc++) {
 			st = new StringTokenizer(br.readLine());
-			int x1 = Integer.parseInt(st.nextToken());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-			
-			for(int y=y1; y<y2; y++) { 
-				for(int x=x1; x<x2; x++){ 
-					square[y][x] = 1;
+			int k1 = Integer.parseInt(st.nextToken());
+			int k2 = Integer.parseInt(st.nextToken());
+			int k3 = Integer.parseInt(st.nextToken());
+			int k4 = Integer.parseInt(st.nextToken());
+			cnt = 0;
+			for (int i = k1; i < k3; i++) {
+				for (int j = k2; j < k4; j++) {
+					board[N - j - 1][i] = 1;
 				}
 			}
 		}
-		
-		ArrayList<Integer> areaCount = new ArrayList<>();
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<m; j++) {
-				if(square[i][j] == 0) {
-					count = 0;
-					dfs(i,j);
-					areaCount.add(count);
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (board[i][j] == 1 || vis[i][j] == true)
+					continue;
+				vis[i][j] = true;
+				cnt++;
+				area = 0;
+				q.add(new Pair(i, j));
+				while (!q.isEmpty()) {
+					Pair cur = q.poll();
+					area++;
+					for (int dir = 0; dir < 4; dir++) {
+						int nx = cur.x + dx[dir];
+						int ny = cur.y + dy[dir];
+						if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+							continue;
+						if (board[nx][ny] == 1 || vis[nx][ny] == true)
+							continue;
+						vis[nx][ny] = true;
+						q.add(new Pair(nx, ny));
+					}
 				}
+				a.add(area);
 			}
+
 		}
-		
-		Collections.sort(areaCount); 
-		
-		sb.append(areaCount.size()).append('\n');  
-		for(int i : areaCount)  {
-			sb.append(i + " ");
+		Collections.sort(a);
+		for (int i = 0; i < a.size(); i++) {
+			sb.append(a.get(i));
+			sb.append(" ");
 		}
-		
+		System.out.println(cnt);
 		System.out.println(sb);
-	}
-	
-	static void dfs(int x, int y) {
-		square[x][y] = 1;
-		count ++; 
-		
-		for(int k=0; k<4; k++) {
-			int nx = x + dx[k];
-			int ny = y + dy[k];
-			
-			if(0<=nx && nx<n && 0<=ny && ny < m) {
-				if(square[nx][ny] == 0) {
-					dfs(nx,ny);
-				}
-			}
-		}
 	}
 
 }
